aa276fe877e246374f94e107b902f62fec4de623
Shrikant Kale <kaleshrikant@live.com>
2025-05-18 14:13:34 +0530

    initial commit

---
+++ src/main/java/com/example/JsonValidation/JsonValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.JsonValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JsonValidation/JsonValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JsonValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
